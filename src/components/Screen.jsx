e9c235006eea4fe27a70b6e10204f6922e9f5a5b
Santiago Fernandez Somellera <a01646946@tec.mx>
2026-04-07 13:32:57 -0600

    class polish of screen right and left controlers

---
+++ src/components/Screen.jsx
@@ -0,0 +1,21 @@
+const Screen = ({ pokemones }) => {
+    return (
+      <div className="w-96 h-80 bg-gray-900 border-8 border-gray-700 rounded-lg flex flex-wrap overflow-y-auto p-2 gap-2"> {/* flex flex wrap lays cards out in ropw the wraps to next line when its*/ }
+        {pokemones.map((pokemon) => (
+          <div
+            key={pokemon.id}
+            className="flex flex-col items-center border border-gray-600 rounded p-1 bg-gray-800"
+          >
+            <img
+              src={pokemon?.sprites?.front_default}
+              className="w-16 h-16"
+              alt={pokemon.name}
+            />
+            <span className="text-white text-xs capitalize">{pokemon.name}</span>
+          </div>
+        ))}
+      </div>
+    );
+  };
+  
+  export default Screen;
