78f95fcaffa024abbd88baef976f6c28262da3fe
Santiago Fernandez Somellera <a01646946@tec.mx>
2026-04-21 14:55:50 -0600

    published to github pages to include the game in the portafolio page

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
+  base: '/console-retro-game/',
   plugins: [
     react(),
     tailwindcss(),
